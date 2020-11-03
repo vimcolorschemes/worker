2e05f9ac58b7673ed02b75bda4ef715edf9da78e
Robin Gagnon <me@reobin.dev>
2020-11-03 16:18:40 -0500

    feat: Fetch repositories from database

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/vimcolorschemes/worker
 
 go 1.15
+
+require (
+	github.com/joho/godotenv v1.3.0
+	go.mongodb.org/mongo-driver v1.4.3
+)
--- main.go
+++ main.go
@@ -2,11 +2,28 @@ package main
 
 import (
 	"fmt"
-	"github.com/vimcolorschemes/worker/job"
+	"log"
 	"os"
+
+	"github.com/joho/godotenv"
+
+	"github.com/vimcolorschemes/worker/job"
+	"github.com/vimcolorschemes/worker/runner"
 )
 
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
+
 func main() {
-	var job = job.GetJob(os.Args)
-	fmt.Println(job)
+	var jobToRun = job.GetJob(os.Args)
+
+	switch jobToRun {
+	case job.Import:
+		import_runner.Run()
+	default:
+		fmt.Println("Error running", jobToRun)
+	}
 }
