c4a5b2d4bb5f8d887d296ca364c346a5eb14387f
Robin Gagnon <me@reobin.dev>
2020-11-04 10:54:41 -0500

    feat(import): Fetch repositories from GitHub

--- cli/import.go
+++ cli/import.go
@@ -1,9 +1,27 @@
 package cli
 
 import (
+	"context"
+	"github.com/google/go-github/v32/github"
 	"log"
 )
 
 func Import() {
 	log.Print("Running import")
+
+	client := github.NewClient(nil)
+
+	repositories := searchRepositories(client)
+
+	log.Print(len(repositories))
+}
+
+func searchRepositories(client *github.Client) []*github.Repository {
+	result, _, err := client.Search.Repositories(context.Background(), "vim color scheme", nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return result.Repositories
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/vimcolorschemes/worker
 go 1.15
 
 require (
+	github.com/google/go-github/v32 v32.1.0
 	github.com/joho/godotenv v1.3.0
 	go.mongodb.org/mongo-driver v1.4.3
 )
