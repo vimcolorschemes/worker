1968ee9a07aba62b5741192ba75f890fcef2b9c4
Robin Gagnon <me@reobin.dev>
2020-11-08 16:55:38 -0500

    refactor: Move import functions to import package

--- cli/import.go
+++ cli/import.go
@@ -1,20 +1,154 @@
 package cli
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"math"
 
 	"github.com/vimcolorschemes/worker/internal/database"
+	"github.com/vimcolorschemes/worker/internal/dotenv"
 	"github.com/vimcolorschemes/worker/internal/github"
+
+	gogithub "github.com/google/go-github/v32/github"
 )
+
+var client *gogithub.Client
+
+var repositoryCountLimit int
+var repositoryCountLimitPerPage int
+
+const searchResultCountHardLimit = 1000
+
+var queryPageCountLimit int = searchResultCountHardLimit
+
+var queries = [10]string{
+	"vim theme",
+	"vim color scheme",
+	"vim colorscheme",
+	"vim colour scheme",
+	"vim colourscheme",
+	"neovim theme",
+	"neovim color scheme",
+	"neovim colorscheme",
+	"neovim colour scheme",
+	"neovim colourscheme",
+}
+
+func init() {
+	client = github.InitGitHubClient()
+
+	repositoryCountLimit = dotenv.GetInt("GITHUB_REPOSITORY_COUNT_LIMIT", false, 100)
+	repositoryCountLimitPerPage = int(math.Min(float64(repositoryCountLimit), 100))
+	queryPageCountLimit = getPageCount(searchResultCountHardLimit, repositoryCountLimitPerPage)
+}
 
 func Import() {
 	log.Print("run import")
 
-	repositories := github.SearchRepositories()
+	repositories := searchRepositories()
 
 	log.Print("Upserting ", len(repositories), " repositories")
 
 	database.UpsertRepositories(repositories)
 
 	log.Print(":wq")
+}
+
+func searchRepositories() []*gogithub.Repository {
+	log.Print("Search repositories")
+
+	var repositories []*gogithub.Repository
+
+	for _, query := range queries {
+		query = fmt.Sprintf("%s %s", query, "NOT dotfiles stars:>0")
+
+		log.Print("query: ", query)
+
+		newRepositories := queryRepositories(query)
+		log.Print("result count: ", len(newRepositories))
+
+		repositories = append(repositories, newRepositories...)
+
+		if len(repositories) >= repositoryCountLimit {
+			break
+		}
+	}
+
+	return uniquifyRepositories(repositories)
+}
+
+func queryRepositories(query string) []*gogithub.Repository {
+	log.Print("page: ", 1)
+
+	result, _, err := client.Search.Repositories(context.Background(), query, buildSearchOptions(1))
+
+	if err != nil {
+		panic(err)
+	}
+
+	repositories := result.Repositories
+	log.Print("result count: ", len(repositories))
+
+	totalCount := result.GetTotal()
+	totalCount = int(math.Min(float64(totalCount), float64(repositoryCountLimit)))
+	log.Print("total count: ", totalCount)
+
+	pageCount := getPageCount(totalCount, repositoryCountLimitPerPage)
+	log.Print("page count: ", pageCount)
+
+	if pageCount == 1 || len(repositories) >= repositoryCountLimit {
+		return repositories
+	}
+
+	return append(repositories, queryPaginatedRepositories(pageCount, query)...)
+}
+
+func queryPaginatedRepositories(pageCount int, query string) []*gogithub.Repository {
+	var repositories []*gogithub.Repository
+
+	for page := 2; page <= pageCount; page++ {
+		log.Print("page: ", page)
+		result, _, err := client.Search.Repositories(context.Background(), query, buildSearchOptions(page))
+
+		if err != nil {
+			panic(err)
+		}
+
+		newRepositories := result.Repositories
+
+		log.Print("result count: ", len(newRepositories))
+
+		repositories = append(repositories, newRepositories...)
+	}
+
+	return repositories
+}
+
+func uniquifyRepositories(repositories []*gogithub.Repository) []*gogithub.Repository {
+	keys := make(map[int64]bool)
+	unique := []*gogithub.Repository{}
+
+	for _, repository := range repositories {
+		if _, value := keys[*repository.ID]; !value {
+			keys[*repository.ID] = true
+			unique = append(unique, repository)
+		}
+	}
+
+	return unique
+}
+
+func getPageCount(totalCount int, itemPerPageCount int) int {
+	pageCount := int(math.Ceil(float64(totalCount / itemPerPageCount)))
+
+	if pageCount > queryPageCountLimit {
+		pageCount = queryPageCountLimit
+	}
+
+	return pageCount
+}
+
+func buildSearchOptions(page int) *gogithub.SearchOptions {
+	return &gogithub.SearchOptions{Sort: "stars", ListOptions: gogithub.ListOptions{PerPage: repositoryCountLimitPerPage, Page: page}}
 }
--- internal/github/github.go
+++ internal/github/github.go
@@ -2,150 +2,22 @@ package github
 
 import (
 	"context"
-	"fmt"
-	"log"
-	"math"
-
-	gogithub "github.com/google/go-github/v32/github"
 
 	"github.com/vimcolorschemes/worker/internal/dotenv"
+
+	gogithub "github.com/google/go-github/v32/github"
 
 	"golang.org/x/oauth2"
 )
 
-var client *gogithub.Client
-
-var repositoryCountLimit int
-var repositoryCountLimitPerPage int
-
-const searchResultCountHardLimit = 1000
-
-var queryPageCountLimit int = searchResultCountHardLimit
-
-var queries = [10]string{
-	"vim theme",
-	"vim color scheme",
-	"vim colorscheme",
-	"vim colour scheme",
-	"vim colourscheme",
-	"neovim theme",
-	"neovim color scheme",
-	"neovim colorscheme",
-	"neovim colour scheme",
-	"neovim colourscheme",
-}
-
-func init() {
-	initGitHubClient()
-
-	repositoryCountLimit = dotenv.GetInt("GITHUB_REPOSITORY_COUNT_LIMIT", false, 100)
-	repositoryCountLimitPerPage = int(math.Min(float64(repositoryCountLimit), 100))
-	queryPageCountLimit = getPageCount(searchResultCountHardLimit, repositoryCountLimitPerPage)
-}
-
-func SearchRepositories() []*gogithub.Repository {
-	log.Print("Search repositories")
-
-	var repositories []*gogithub.Repository
-
-	for _, query := range queries {
-		query = fmt.Sprintf("%s %s", query, "NOT dotfiles stars:>0")
-
-		log.Print("query: ", query)
-
-		newRepositories := queryRepositories(query)
-		log.Print("result count: ", len(newRepositories))
-
-		repositories = append(repositories, newRepositories...)
-
-		if len(repositories) >= repositoryCountLimit {
-			break
-		}
-	}
-
-	return uniquifyRepositories(repositories)
-}
-
-func initGitHubClient() {
+func InitGitHubClient() *gogithub.Client {
 	ctx := context.Background()
+
 	ts := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: dotenv.Get("GITHUB_TOKEN", true)},
 	)
+
 	tc := oauth2.NewClient(ctx, ts)
-	client = gogithub.NewClient(tc)
-}
 
-func queryRepositories(query string) []*gogithub.Repository {
-	log.Print("page: ", 1)
-
-	result, _, err := client.Search.Repositories(context.Background(), query, buildSearchOptions(1))
-
-	if err != nil {
-		panic(err)
-	}
-
-	repositories := result.Repositories
-	log.Print("result count: ", len(repositories))
-
-	totalCount := result.GetTotal()
-	totalCount = int(math.Min(float64(totalCount), float64(repositoryCountLimit)))
-	log.Print("total count: ", totalCount)
-
-	pageCount := getPageCount(totalCount, repositoryCountLimitPerPage)
-	log.Print("page count: ", pageCount)
-
-	if pageCount == 1 || len(repositories) >= repositoryCountLimit {
-		return repositories
-	}
-
-	return append(repositories, queryPaginatedRepositories(pageCount, query)...)
-}
-
-func queryPaginatedRepositories(pageCount int, query string) []*gogithub.Repository {
-	var repositories []*gogithub.Repository
-
-	for page := 2; page <= pageCount; page++ {
-		log.Print("page: ", page)
-		result, _, err := client.Search.Repositories(context.Background(), query, buildSearchOptions(page))
-
-		if err != nil {
-			panic(err)
-		}
-
-		newRepositories := result.Repositories
-
-		log.Print("result count: ", len(newRepositories))
-
-		repositories = append(repositories, newRepositories...)
-	}
-
-	return repositories
-}
-
-func uniquifyRepositories(repositories []*gogithub.Repository) []*gogithub.Repository {
-	keys := make(map[int64]bool)
-	unique := []*gogithub.Repository{}
-
-	for _, repository := range repositories {
-		if _, value := keys[*repository.ID]; !value {
-			keys[*repository.ID] = true
-			unique = append(unique, repository)
-		}
-	}
-
-	return unique
-}
-
-func getPageCount(totalCount int, itemPerPageCount int) int {
-	pageCount := int(math.Ceil(float64(totalCount / itemPerPageCount)))
-
-	if pageCount > queryPageCountLimit {
-		pageCount = queryPageCountLimit
-	}
-
-	return pageCount
-}
-
-func buildSearchOptions(page int) *gogithub.SearchOptions {
-	return &gogithub.SearchOptions{Sort: "stars", ListOptions: gogithub.ListOptions{PerPage: repositoryCountLimitPerPage, Page: page}}
+	return gogithub.NewClient(tc)
 }
