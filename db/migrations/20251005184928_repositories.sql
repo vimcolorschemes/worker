0c29b0eb76b445811a38c3042fd194cf7a89bbb9
Robin Gagnon <me@reobin.dev>
2025-10-05 18:54:06 -0500

    build: Add DB setup script and migrations

---
+++ db/migrations/20251005184928_repositories.sql
@@ -0,0 +1,9 @@
+CREATE TABLE repositories (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  owner TEXT NOT NULL,
+  description TEXT,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
+  UNIQUE(owner, name)
+);
