0adeda0ae56ca6e32383677fac4a4098b0c14b10
Robin Gagnon <me@reobin.dev>
2025-10-12 21:00:07 -0500

    refactor(db): Use JSON format and add missing FK to colorscheme data

--- db/migrations/20251005195216_colorschemes.sql
+++ db/migrations/20251005195216_colorschemes.sql
@@ -7,9 +7,9 @@ CREATE TABLE colorschemes (
 
 CREATE TABLE colorscheme_color_data (
   colorscheme_name TEXT NOT NULL,
-  repository_id INTEGER NOT NULL,
+  repository_id INTEGER NOT NULL REFERENCES repositories(id) ON DELETE CASCADE,
   background TEXT NOT NULL CHECK (background IN ('light', 'dark')),
-  color_data TEXT NOT NULL,
+  color_data JSON NOT NULL,
 
   PRIMARY KEY (colorscheme_name, repository_id, background)
 )
